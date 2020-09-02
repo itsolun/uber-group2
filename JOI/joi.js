19919fd30722868796b425229da56f468a2967e6
MohamedAlsisi <mhmd.alsisi@gmail.com>
2020-09-03 01:28:33 +0200

    master

---
+++ JOI/joi.js
@@ -0,0 +1,128 @@
+const Joi = require('joi');
+
+const UsersJoiSchema = Joi.object(
+    {
+        username: Joi.string()
+            .alphanum()
+            .min(5)
+            .max(25)
+            .required(),
+
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+            .required(),
+
+        email: Joi.string()
+            .pattern(new RegExp('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$'))
+            .required(),
+
+        // token: Joi.string().number().required(),
+        // current_location: Joi.required(),
+
+        mobile_number: Joi.number()
+            .pattern(new RegExp('(201)[0-9]{9}'))
+            .required(),
+
+        status: Joi.boolean()
+    }
+);
+
+const DriversJoiSchema = Joi.object(
+    {
+        username: Joi.string()
+            .alphanum()
+            .min(5)
+            .max(25)
+            .required(),
+
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+            .required(),
+
+        email: Joi.string()
+            .pattern(new RegExp('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$'))
+            .required(),
+
+        // token: Joi.string().number().required(),
+        // current_location: Joi.required(),
+
+        mobile_number: Joi.number()
+            .pattern(new RegExp('(201)[0-9]{9}'))
+            .required(),
+
+        status: Joi.boolean(),
+
+        current_car: Joi.ref(CarJoiSchema)
+    }
+);
+
+const CarsJoiSchema = Joi.object(
+    {
+        car_model: Joi.string()
+            .required(),
+
+        car_year: Joi.number()
+            .required(),
+
+        license: Joi.string(),
+
+        plate_numbers: Joi.string()
+            .required(),
+
+        driver: Joi.ref(DriversJoiSchema)
+    }
+);
+
+const TripsJoiSchema = Joi.object(
+    {
+        start_point: Joi.required(),
+
+        destination: Joi.required(),
+
+        arrival_time: Joi.string()
+            .pattern(new RegExp('^([0-9]{2})\:([0-9]{2})$'))
+            .required(),
+
+        start_time: Joi.string().pattern(new RegExp('^([0-9]{2})\:([0-9]{2})$')).required(),
+
+        cost: Joi.number()
+            .required(),
+
+        rider: Joi.ref(UsersJoiSchema),
+
+        driver: Joi.ref(DriversJoiSchema)
+    }
+);
+
+module.exports.UsersJoiSchema = UsersJoiSchema;
+module.exports.DriversJoiSchema = DriversJoiSchema;
+module.exports.TripsJoiSchema = TripsJoiSchema;
+module.exports.CarsJoiSchema = CarsJoiSchema;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ db/dataModel.js
@@ -0,0 +1,73 @@
+mongoose = require('mongoose')
+
+const users_schema = mongoose.Schema({
+    _id = mongoose.Types.ObjectId,
+    username: { type: String, unique: true, required: true, trim: true },
+    password: String,
+    email: {type: String, unique: true},
+    // token: 
+    current_location: {
+		type: {type: String, required: true, default: "Point"},
+		coordinates: [ Number ]
+	},     
+    mobile_number:{type: Number},
+    status: {type: Boolean},
+    trips:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "trips"
+      }]
+
+});
+
+const drivers_schema = mongoose.Schema({
+    _id = mongoose.Types.ObjectId,	
+    username: { type: String, unique: true, required: true, trim: true },
+    password: String,
+    email   : {type: String, unique: true},
+    // token	
+    current_location: {
+		type: {type: String, required: true, default: "Point"},
+		coordinates: [ Number ],
+    },   
+       
+    available: {type: Boolean},
+    mobile_number: Number,
+    status: {type: Boolean},	
+    current_car: {
+        car_model: {type: String},
+        car_year: {type: Number},
+        license: {type: String},
+        plat_numbers: {type: String}
+    },	
+    trips:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "trips"
+      }]
+});
+
+const trips_schema = mongoose.Schema({
+    _id = mongoose.Types.ObjectId,
+    start_point:  {
+		type: { type: String, required: true, default: "Point" },
+		coordinates: [ Number ],
+	},       
+    destination: {
+		type: { type: String, required: true, default: "Point" },
+		coordinates: [ Number ],
+    },
+    
+    arrival_time: Date,
+    start_time: Date,
+    cost: Number,
+    driver_id: mongoose.Types.ObjectId,
+    rider_id: _id = mongoose.Types.ObjectId
+
+});
+
+const users = mongoose.model("users", users_schema);
+const drivers = mongoose.model("drivers", drivers_schema);
+const trips = mongoose.model("trips", trips_schema);
+
+module.exports = users;
+module.exports = drivers;
+module.exports = trips;
